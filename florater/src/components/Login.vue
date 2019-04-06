b34eeabe8390b9b1a3c60b937409094055e9851a
Artur Białoskórski <bialoskorski.artur@gmail.com>
2019-04-06 14:14:30 +0200

    dostosowano nazwy zmiennych danych do nazw z bazy

--- florater/src/components/Login.vue
+++ florater/src/components/Login.vue
@@ -19,57 +19,57 @@
       @input="$v.password.$touch()"
       @blur="$v.password.$touch()"
     ></v-text-field>
-
     <v-btn @click="login">Zaloguj się</v-btn>
     <v-btn @click="clear">Wyczyść</v-btn>
   </form>
 </template>
 
 <script>
-import { required, maxLength, minLength } from 'vuelidate/lib/validators'
-import { validationMixin } from 'vuelidate'
-
+import { required, maxLength, minLength } from "vuelidate/lib/validators";
+import { validationMixin } from "vuelidate";
 
 export default {
-mixins: [validationMixin],
-validations: {
-  email: { required, minLength: minLength(2), maxLength: maxLength(30) },
-  password: { required , minLength: minLength(4) }
-},
-data() {
-  return {
-    email: "",
-    password: ""
-  };
-},
+  mixins: [validationMixin],
+  validations: {
+    email: { required, minLength: minLength(1), maxLength: maxLength(30) },
+    password: { required, minLength: minLength(1) }
+  },
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
 
-
-computed: {
-      emailErrors () {
-        const errors = []
-        if (!this.$v.email.$dirty) return errors
-        !this.$v.email.minLength && errors.push('Password must be at least 2 characters long')
-        !this.$v.email.maxLength && errors.push('E-mail must be at most 30 characters long')
-        !this.$v.email.required && errors.push('E-mail is required')
-        return errors
-      },
-      passwordErrors () {
-        const errors = []
-        if (!this.$v.password.$dirty) return errors
-        !this.$v.password.minLength && errors.push('Password must be at least 4 characters long')
-        !this.$v.password.required && errors.push('Password is required')
-        return errors
-      }
+  computed: {
+    emailErrors() {
+      const errors = [];
+      if (!this.$v.email.$dirty) return errors;
+      !this.$v.email.minLength &&
+        errors.push("Password must be at least 1 character long");
+      !this.$v.email.maxLength &&
+        errors.push("E-mail must be at most 30 characters long");
+      !this.$v.email.required && errors.push("E-mail is required");
+      return errors;
     },
-
-    methods: {
-      clear () {
-        this.$v.$reset()
-        this.email = ""
-        this.password = ""
-      }
+    passwordErrors() {
+      const errors = [];
+      if (!this.$v.password.$dirty) return errors;
+      !this.$v.password.minLength &&
+        errors.push("Password must be at least 1 character long");
+      !this.$v.password.required && errors.push("Password is required");
+      return errors;
     }
-}
+  },
+
+  methods: {
+    clear() {
+      this.$v.$reset();
+      this.email = "";
+      this.password = "";
+    }
+  }
+};
 </script>
 
 
--- florater/src/components/Register.vue
+++ florater/src/components/Register.vue
@@ -2,23 +2,23 @@
   <form>
     <v-text-field
       label="Imię"
-      v-model="name"
-      :error-messages="nameErrors"
+      v-model="first_name"
+      :error-messages="first_nameErrors"
       :counter="20"
       autocomplete="on"
       required
-      @input="$v.name.$touch()"
-      @blur="$v.name.$touch()"
+      @input="$v.first_name.$touch()"
+      @blur="$v.first_name.$touch()"
     ></v-text-field>
     <v-text-field
       label="Nazwisko"
-      v-model="surname"
-      :error-messages="surnameErrors"
+      v-model="last_name"
+      :error-messages="last_nameErrors"
       :counter="30"
       autocomplete="on"
       required
-      @input="$v.surname.$touch()"
-      @blur="$v.surname.$touch()"
+      @input="$v.last_name.$touch()"
+      @blur="$v.last_name.$touch()"
     ></v-text-field>
     <v-text-field
       label="E-mail"
@@ -46,72 +46,77 @@
 </template>
 
 <script>
-import { required, maxLength, minLength } from 'vuelidate/lib/validators'
-import { validationMixin } from 'vuelidate'
-
+import { required, maxLength, minLength } from "vuelidate/lib/validators";
+import { validationMixin } from "vuelidate";
 
 export default {
-mixins: [validationMixin],
-validations: {
-  name: { required, minLength: minLength(2), maxLength: maxLength(20) },
-  surname: { required, minLength: minLength(2), maxLength: maxLength(30) },
-  email: { required, minLength: minLength(2), maxLength: maxLength(30) },
-  password: { required, minLength: minLength(4) }
-},
-data() {
-  return {
-    name: "",
-    surname: "",
-    email: "",
-    password: ""
-  };
-},
+  mixins: [validationMixin],
+  validations: {
+    first_name: { required, minLength: minLength(1), maxLength: maxLength(20) },
+    last_name: { required, minLength: minLength(1), maxLength: maxLength(30) },
+    email: { required, minLength: minLength(1), maxLength: maxLength(30) },
+    password: { required, minLength: minLength(1) }
+  },
+  data() {
+    return {
+      first_name: "",
+      last_name: "",
+      email: "",
+      password: ""
+    };
+  },
 
-
-computed: {
-      nameErrors () {
-        const errors = []
-        if (!this.$v.name.$dirty) return errors
-        !this.$v.name.minLength && errors.push('Name must be at least 2 characters long')
-        !this.$v.name.maxLength && errors.push('Name must be at most 20 characters long')
-        !this.$v.name.required && errors.push('Name is required')
-        return errors
-      },
-      surnameErrors () {
-        const errors = []
-        if (!this.$v.surname.$dirty) return errors
-        !this.$v.surname.minLength && errors.push('Surname must be at least 2 characters long')
-        !this.$v.surname.maxLength && errors.push('Surname must be at most 30 characters long')
-        !this.$v.surname.required && errors.push('Surname is required')
-        return errors
-      },
-        emailErrors () {
-          const errors = []
-          if (!this.$v.email.$dirty) return errors
-          !this.$v.email.minLength && errors.push('E-mail must be at least 2 characters long')
-          !this.$v.email.maxLength && errors.push('E-mail must be at most 30 characters long')
-          !this.$v.email.required && errors.push('E-mail is required')
-          return errors
-        },
-      passwordErrors () {
-        const errors = []
-        if (!this.$v.password.$dirty) return errors
-        !this.$v.password.required && errors.push('Password is required')
-        !this.$v.password.minLength && errors.push('Password must be at least 4 characters long')
-        return errors
-      }
+  computed: {
+    first_nameErrors() {
+      const errors = [];
+      if (!this.$v.first_name.$dirty) return errors;
+      !this.$v.first_name.minLength &&
+        errors.push("first_name must be at least 1 character long");
+      !this.$v.first_name.maxLength &&
+        errors.push("first_name must be at most 20 characters long");
+      !this.$v.first_name.required && errors.push("first_name is required");
+      return errors;
     },
-
-    methods: {
-      clear () {
-        this.$v.$reset()
-        this.name = ""
-        this.surname = ""
-        this.email = ""
-        this.password = ""
-      }
+    last_nameErrors() {
+      const errors = [];
+      if (!this.$v.last_name.$dirty) return errors;
+      !this.$v.last_name.minLength &&
+        errors.push("last_name must be at least 1 character long");
+      !this.$v.last_name.maxLength &&
+        errors.push("last_name must be at most 30 characters long");
+      !this.$v.last_name.required && errors.push("last_name is required");
+      return errors;
+    },
+    emailErrors() {
+      const errors = [];
+      if (!this.$v.email.$dirty) return errors;
+      !this.$v.email.minLength &&
+        errors.push("E-mail must be at least 1 character long");
+      !this.$v.email.maxLength &&
+        errors.push("E-mail must be at most 30 characters long");
+      !this.$v.email.required && errors.push("E-mail is required");
+      return errors;
+    },
+    passwordErrors() {
+      const errors = [];
+      if (!this.$v.password.$dirty) return errors;
+      !this.$v.password.required && errors.push("Password is required");
+      !this.$v.password.minLength &&
+        errors.push("Password must be at least 1 character long");
+      return errors;
     }
-}
+  },
+
+  methods: {
+    clear() {
+      this.$v.$reset();
+      this.first_name = "";
+      this.last_name = "";
+      this.email = "";
+      this.password = "";
+    }
+  }
+};
 </script>
 
 <style scoped lang="scss">
