f50689ea3c431c59679ff06ff347241a516b81ab
Artur Białoskórski <bialoskorski.artur@gmail.com>
2019-05-15 19:22:02 +0200

    calendar added

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -24,7 +24,7 @@
             <v-flex xs12 sm6>
               <v-slider
                 v-model="resources"
-                color="orange"
+                color="green"
                 label="Zasoby"
                 min="1"
                 max="3"
@@ -38,13 +38,47 @@
               required
               placeholder="52º 28' 0.337 N 16º 55' 36.991 E"
             ></v-text-field>
-            <v-text-field ref="date" v-model="date" label="Data" required placeholder="24.04.2019"></v-text-field>
+
+            <div class="form__calendar">
+              <v-layout row wrap>
+                <v-flex xs12 sm6 md4>
+                  <v-dialog
+                    ref="dialog"
+                    v-model="modal"
+                    :return-value.sync="date"
+                    persistent
+                    lazy
+                    width="290px"
+                  >
+                    <template v-slot:activator="{ on }">
+                      <v-text-field
+                        v-model="date"
+                        label="Wybierz datę z kalendarza"
+                        prepend-icon="event"
+                        readonly
+                        v-on="on"
+                      ></v-text-field>
+                    </template>
+                    <v-date-picker v-model="date" scrollable>
+                      <v-btn flat color="primary" @click="modal = false">Cancel</v-btn>
+                      <v-btn flat color="primary" @click="$refs.dialog.save(date)">OK</v-btn>
+                    </v-date-picker>
+                  </v-dialog>
+                </v-flex>
+              </v-layout>
+            </div>
+
             <v-flex xs12 sm6 md3>
-              <v-text-field label="Informacje dodatkowe" background-color="grey" outline></v-text-field>
+              <v-text-field
+                class="form__description"
+                label="Informacje dodatkowe"
+                background-color="grey"
+                outline
+              ></v-text-field>
             </v-flex>
           </v-card-text>
           <v-divider class="mt-5"></v-divider>
-          <v-card-actions>
+          <v-card-actions class="form__add">
             <v-spacer></v-spacer>
             <v-btn color="primary" @click="submit">Dodaj</v-btn>
           </v-card-actions>
@@ -56,11 +90,21 @@
 
 <script>
 export default {
-  data() {
-    return {};
-  }
+  data: () => ({
+    date: new Date().toISOString().substr(0, 10),
+    menu: false,
+    modal: false,
+    menu2: false
+  })
 };
 </script>
 
 <style scoped lang="scss">
+.form__calendar {
+  margin-top: 20px;
+}
+.form__description {
+  margin-top: 80px;
+  width: 500px;
+}
 </style>
